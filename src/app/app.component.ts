88ad90dc7ad2474ec0d494f7dd27d1c569ceb81d
Theeprased Meemui <b5711512@office365.sut.ac.th>
2021-10-21 22:52:55 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'pretest-section2-question2';
+}
